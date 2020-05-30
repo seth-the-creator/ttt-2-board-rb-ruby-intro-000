885619840eddf541a74b71943c436d6844434f3c
Seth Mueller <sethmueller1@gmail.com>
2020-05-30 05:07:35 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,3 +1,7 @@
 # Define the variable board below.
 # row = [" ", " ", " "]
+<<<<<<< HEAD
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+=======
+board = [" ", " ", " "]
+>>>>>>> 58e7b9f95a8e3e07d0464a499dafbf27fd2ddb85
